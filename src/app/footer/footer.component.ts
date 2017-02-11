69fd888f4102129c7d5fde5bbf059ea8e03f9acd
Cash Wu <cash@cashwu.com>
2017-02-11 15:03:06 +0800

    任務 10：套用 @Input() 裝飾器 (Decorator)

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  @Input() 
+  todos: any[];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+ get todoLength() {
+    return this.todos.filter(a => !a.done).length;
+ }
+}
